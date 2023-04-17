5c0d9d89c82bb5cbf5d587608a913287f3674634
yday05506 <yangdayeon2@gmail.com>
2023-04-17 11:11:49 +0900

    프로필, 회원가입 완료 레이아웃

---
+++ app/src/main/java/com/example/moody/SignFinishActivity.java
@@ -0,0 +1,14 @@
+package com.example.moody;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SignFinishActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_finish);
+    }
+}
