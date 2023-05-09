b7c389325322b05ea9e776f02b3b320a0d5dc5ba
yday05506 <yangdayeon2@gmail.com>
2023-05-10 01:24:22 +0900

    장바구니 레이아웃 구현

---
+++ app/src/main/java/com/example/moody/MarketCartActivity.java
@@ -0,0 +1,14 @@
+package com.example.moody;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MarketCartActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_market_cart);
+    }
+}
