4c157741c7fee781f957add91cb4ef9f6b751a6e
yday05506 <yangdayeon2@gmail.com>
2023-06-09 14:41:32 +0900

    홈 레이아웃

--- app/src/main/java/com/example/moody/MainActivity.java
+++ app/src/main/java/com/example/moody/MainActivity.java
@@ -1,12 +1,17 @@
 package com.example.moody;
 
 import android.os.Bundle;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
+import androidx.viewpager2.widget.ViewPager2;
 
 import com.example.moody.databinding.ActivityMainBinding;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -15,6 +20,14 @@ public class MainActivity extends AppCompatActivity {
 
     ActivityMainBinding binding;
     FloatingActionButton moody;
+
+    private ViewPager2 sliderViewPager;
+    private LinearLayout layoutIndicator;
+
+    private String[] images = new String[] {
+            "https://www.figma.com/file/UmCsMspS1dVnSZ0Vcm12bl/%23moody?type=design&node-id=1500-202&t=fgX5azEvpiYwIJvb-4",
+            "https://www.figma.com/file/UmCsMspS1dVnSZ0Vcm12bl/%23moody?type=design&node-id=1479-203&t=fgX5azEvpiYwIJvb-4"
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -59,6 +72,32 @@ public class MainActivity extends AppCompatActivity {
         ActionBar actionBar = getSupportActionBar();
         actionBar.hide();
 
+        sliderViewPager = findViewById(R.id.sliderViewPager);
+        layoutIndicator = findViewById(R.id.layoutIndicators);
+
+        sliderViewPager.setOffscreenPageLimit(1);
+        sliderViewPager.setAdapter(new ImageSliderAdapter(this, images));
+
+        sliderViewPager.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+            @Override
+            public void onPageSelected(int position) {
+                super.onPageSelected(position);
+            }
+        });
+        setupIndicators(images.length);
+    }
+
+    private void setupIndicators(int count) {
+        ImageView[] indicators = new ImageView[count];
+        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+
+        params.setMargins(16,8,16,8);
+
+        for(int i = 0; i < indicators.length; i++) {
+            indicators[i] = new ImageView(this);
+            indicators[i].setLayoutParams(params);
+            layoutIndicator.addView(indicators[i]);
+        }
     }
 
     // 화면 전환
