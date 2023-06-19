bc216342cec6792252f55af429852d8d5754b9b3
yday05506 <yangdayeon2@gmail.com>
2023-06-19 11:54:15 +0900

    마켓 리사이클러뷰 추가

--- app/src/main/java/com/example/moody/WriteReviewActivity.java
+++ app/src/main/java/com/example/moody/WriteReviewActivity.java
@@ -2,13 +2,33 @@ package com.example.moody;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import io.github.muddz.styleabletoast.StyleableToast;
 
 public class WriteReviewActivity extends AppCompatActivity {
+    Button addReviewBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_write_review);
+
+        addReviewBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Context context = WriteReviewActivity.this;
+                new StyleableToast.Builder(context).text("후기를 등록하였습니다!").iconStart(R.drawable.moody).length(Toast.LENGTH_SHORT).show();
+
+                Intent intent = new Intent(getApplicationContext(), Review.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }
