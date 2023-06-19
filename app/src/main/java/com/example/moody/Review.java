d41755b94678e837a20121f0cddf26a12e26b498
yday05506 <yangdayeon2@gmail.com>
2023-06-19 13:19:01 +0900

    리뷰 버튼 이동, sign up, sign in 레이아웃 변경

--- app/src/main/java/com/example/moody/Review.java
+++ app/src/main/java/com/example/moody/Review.java
@@ -12,8 +12,11 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
 public class Review extends Fragment {
     private ImageButton btn_search;
+    private FloatingActionButton btn_add_review;
 
 
 
@@ -33,6 +36,17 @@ public class Review extends Fragment {
                 startActivity(intent);
             }
         });
+
+        btn_add_review = view.findViewById(R.id.btn_add_review);
+        btn_add_review.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getActivity(), WriteReviewActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+                startActivity(intent);
+            }
+        });
+
         return view;
     }
 }
