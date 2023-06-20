2f0b84d6ff0628663b9df7b8f71d56501dfd78d4
yday05506 <yangdayeon2@gmail.com>
2023-06-20 11:33:02 +0900

    Bottom Sheet 증가, 감소

--- app/src/main/java/com/example/moody/BottomSheetDialog.java
+++ app/src/main/java/com/example/moody/BottomSheetDialog.java
@@ -6,11 +6,16 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
+
+import io.github.muddz.styleabletoast.StyleableToast;
 
 public class BottomSheetDialog extends BottomSheetDialogFragment {
     private View view;
@@ -18,10 +23,17 @@ public class BottomSheetDialog extends BottomSheetDialogFragment {
     private Button btn_cart;
     private Button btn_buy;
 
+    private ImageButton btnMinus;
+    private ImageButton btnPlus;
+    private TextView numTextView;
+
+    private int num = 0;    // 상품 개수
+
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.bottom_sheet, container, false);
 
         mListener = (BottomSheetListener) getContext();
+        numTextView.setText(num+"");
 
         btn_cart = view.findViewById(R.id.btn_cart);
         btn_buy = view.findViewById(R.id.btn_buy);
@@ -39,6 +51,30 @@ public class BottomSheetDialog extends BottomSheetDialogFragment {
             public void onClick(View v) {
                 mListener.onButtonClicked("바로 구매");
                 dismiss();
+            }
+        });
+
+        btnMinus = view.findViewById(R.id.btn_minus);
+        btnPlus = view.findViewById(R.id.btn_plus);
+
+        btnMinus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Context context = getContext();
+                if(num < 0) {
+                    new StyleableToast.Builder(context).text("오픈 전인 서비스입니다!").iconStart(R.drawable.moody).length(Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    num--;
+                    numTextView.setText(num+"");
+                }
+            }
+        });
+        btnPlus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                num++;
+                numTextView.setText(num+"");
             }
         });
 
